916af7f41993b8898dcbf20b62f305811a17f25e
Lander Van laer <lander.vanlaer1@gmail.com>
2023-09-26 20:08:31 +0200

    Init & Labo 1.1

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1
+{
+    internal class Program
+    {
+        /**
+         * Write a C# console application that asks for your name and print Hello and your name.
+         * You can also give your name as an argument, in which case the user is not prompted for the name anymore.
+         */
+        public static void Main(string[] args)
+        {
+            string name;
+            if (args.Length >= 1)
+            {
+                name = args[0];
+            }
+            else
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
---
+++ c-sharp-dotnet-development-lab-3AI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace c_sharp_dotnet_development_lab_3AI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
